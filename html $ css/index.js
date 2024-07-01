dcf84de35f5f999a9ae2fd1a9f415ef33b808cb3
Marla-lalitha04 <marlalalitha04@gmail.com>
2024-07-01 12:04:32 +0530

    using html & js

---
+++ html $ css/index.js
@@ -0,0 +1,31 @@
+let name;
+document.getElementById("btn1").onclick=function(){
+    console.log("firstname is clicked")
+    }
+    document.getElementById("btn2").onclick=function(){
+        console.log("middlename is clicked")
+        }
+        document.getElementById("btn3").onclick=function(){
+            console.log("lastname is clicked")
+            }
+            document.getElementById("btn4").onclick=function(){
+                console.log("course is clicked")
+                }
+                document.getElementById("btn5").onclick=function(){
+                    console.log("genderis clicked")
+                    }
+                    document.getElementById("btn6").onclick=function(){
+                        console.log("phone no is clicked")
+                        }
+                        document.getElementById("btn7").onclick=function(){
+                            console.log("adress is clicked")
+                            }
+                            document.getElementById("btn8").onclick=function(){
+                                console.log("email is clicked")
+                                }
+                                document.getElementById("btn9").onclick=function(){
+                                    console.log("password is clicked")
+                                    }
+                                    document.getElementById("btn10").onclick=function(){
+                                        console.log("re-type password is clicked")
+                                        }
